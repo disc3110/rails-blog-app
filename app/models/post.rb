eabd78518d9686b865aa4c7df75b2b1cd6e9891b
Diego Solis <disc3110@gmail.com>
2022-03-28 09:58:24 -0600

    add methods to update author posts and see last 5

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
+
+  def update_author_posts
+    counter = author.posts.count('id')
+    author.update(posts_counter: counter)
+  end
+
+  def five_recent_posts
+    comments.order(created_at: :desc).limit(5)
+  end
 end
