35f63cbde906e0cabb7b1f8b082df537857f5df5
Diego Solis <disc3110@gmail.com>
2022-03-28 09:59:30 -0600

    add methods to update post comments and see last 3

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,13 @@
 class Comment < ApplicationRecord
-  belongs_to :author_id
-  belongs_to :post_id
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
+
+  def three_recent_posts
+    posts.order(created_at: :desc).limit(quantity)
+  end
+
+  def update_post_comments
+    counter = post.comments.count('id')
+    post.update(comments_counter: counter)
+  end
 end
