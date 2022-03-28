0d53a9669fb239dc3c36715f562bff48c64d3f5a
Diego Solis <disc3110@gmail.com>
2022-03-28 09:57:19 -0600

    change user id for author id

---
+++ db/migrate/20220324231921_rename_user_to_author.rb
@@ -0,0 +1,15 @@
+class RenameUserToAuthor < ActiveRecord::Migration[7.0]
+  def change
+    change_table :posts do |t|
+      t.rename :user_id, :author_id
+    end
+
+    change_table :comments do |t|
+      t.rename :user_id, :author_id
+    end
+
+    change_table :likes do |t|
+      t.rename :user_id, :author_id
+    end
+  end
+end
