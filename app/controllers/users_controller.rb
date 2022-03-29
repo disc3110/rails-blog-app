58503cdf35082304dd8d86d92d57f0c892ade6eb
Diego Solis <disc3110@gmail.com>
2022-03-28 20:37:58 -0600

    fix linters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
   def index
-   @shown_users = User.take(3)
+    @shown_users = User.take(3)
   end
 
   def show
-   @shown_user = User.find(params[:id])
-   @recent_posts = @shown_user.three_recent_posts
+    @shown_user = User.find(params[:id])
+    @recent_posts = @shown_user.three_recent_posts
   end
 end
