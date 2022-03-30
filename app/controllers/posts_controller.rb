9b5411b1c0102ff8512195b765df0aaab364d331
Diego Solis <disc3110@gmail.com>
2022-03-29 22:57:25 -0600

    add method to add new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,36 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @comments = @post.comments.all
   end
+
+  def new
+    @user = current_user
+  end
+
+  def create
+    @user = current_user
+    p(post_params)
+    @post = @user.posts.new(post_params)
+    p(@post)
+
+    if @post.save
+      redirect_to user_post_path(@user, @post)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def like
+    @post = Post.all.find(params[:id])
+    @user = User.find(params[:user_id])
+    @current_user = current_user
+    @like = @current_user.likes.new(post_id: @post.id)
+    @like.save
+    redirect_to user_post_path(@user, @post)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,12 +3,16 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  def update_author_posts
-    counter = author.posts.count('id')
-    author.update(posts_counter: counter)
-  end
+  after_save :update_author_posts
 
   def five_recent_comments
     comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def update_author_posts
+    counter = author.posts.count('id')
+    author.update(postsCounter: counter)
   end
 end
