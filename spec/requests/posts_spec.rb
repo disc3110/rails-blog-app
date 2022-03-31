dedf57ab4de6c2f5aff21543f72cabf333a34bd5
Diego Solis <disc3110@gmail.com>
2022-03-31 07:58:06 -0600

    Update view tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe 'Posts', type: :request do
       expect(response).to render_template('index')
     end
     it 'body has the correct text' do
-      expect(response.body).to include('Post #1')
+      expect(response.body).to include('RailsBlogApp')
     end
   end
 
@@ -23,7 +23,7 @@ RSpec.describe 'Posts', type: :request do
       expect(response).to render_template('show')
     end
     it 'body has the correct text' do
-      expect(response.body).to include('Post #1 by username')
+      expect(response.body).to include('RailsBlogApp')
     end
   end
 end
