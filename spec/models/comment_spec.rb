b8d653248f8283e969eeaf109b2e3cbbbfb400ca
Diego Solis <disc3110@gmail.com>
2022-03-31 07:59:20 -0600

    Add test for Comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    user = User.find(1)
+    post = Post.find(1)
+    @comment = Comment.new(author: user, post: post, text: 'Hello, how are you?')
+  end
+
+  it 'comment with correct parameters should be valid' do
+    expect(@comment).to be_valid
+  end
 end
