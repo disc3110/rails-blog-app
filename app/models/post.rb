ffa0ddc61e58404446fe63bbd7b92b9f4b2a0e92
Diego Solis <disc3110@gmail.com>
2022-03-31 07:55:58 -0600

    Write post valiodations

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :commentsCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likesCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   after_save :update_author_posts
 
   def five_recent_comments
